702e05d58acfce28ca88b659dded4e94dc5a18ad
Soumyajit Sarkar <soumyajit.s@testyantra.in>
2022-04-28 16:18:52 +0530

    Spring cloud with security

---
+++ course-service/src/main/java/com/te/course/service/Course.java
@@ -0,0 +1,16 @@
+package com.te.course.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    private String id;
+    private String name;
+    private int durationInMonths;
+    private int price;
+
+}
